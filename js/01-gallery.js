51912dad97cc09bcac2486972c352ab8768cfed2
Silence-tss <99872463+Silence-tss@users.noreply.github.com>
2023-08-31 23:41:35 +0300

    test

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+galleryItems.forEach((image) = {
+    
+})
+
+
+
+// const gallery = document.querySelector('.gallery');
+// const modal = document.querySelector('.modal');
+// const modalImage = document.querySelector('.modal__image');
+// const modalClose = document.querySelector('.modal__close');
+
+// gallery.addEventListener('click', (event) => {
+//       event.preventDefault();
+
+//       if (event.target.classList.contains('gallery__image')) {
+//         const largeImageSrc = event.target.getAttribute('data-source');
+//         modalImage.src = largeImageSrc;
+//         modal.style.display = 'block';
+//       }
+//     });
+
+//     modalClose.addEventListener('click', () => {
+//       modal.style.display = 'none';
+//     });
+
+//     modal.addEventListener('click', (event) => {
+//       if (event.target === modal) {
+//         modal.style.display = 'none';
+//       }
+//     });
+
