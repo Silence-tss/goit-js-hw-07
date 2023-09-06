eb10578714a503fe80e8b61dd29bbcdbe8563849
Silence-tss <99872463+Silence-tss@users.noreply.github.com>
2023-09-06 17:52:27 +0300

    Task 1, task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,45 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
 
-const previewImageUrlEl = document.querySelector(".gallery__link");
+
+const galleryEl = document.querySelector(".gallery");
+const galleryItemEl = galleryItems.map(item => 
+  `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+    </a>
+  </li>`
+  ).join(" ");
 
 
+  galleryEl.innerHTML = galleryItemEl;
 
-galleryItems.forEach((image) => {
-    const previewImageUrl = image.preview;
-    
 
-    const originalImageUrl = image.original;
-    const descriptionImageAlt = image.description;
-    console.log(previewImageUrl);
-    console.log(originalImageUrl);
-    console.log(descriptionImageAlt);
+  galleryEl.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    if (evt.target.tagName === "IMG") {
+      const originalImageEl = evt.target.getAttribute("data-source");
+
+      const instance = basicLightbox.create(`
+      <img src="${originalImageEl}" alt="${evt.target.alt}" />`)
+
+      instance.show();
+    }
   });
-
-
-
-// const gallery = document.querySelector('.gallery');
-// const modal = document.querySelector('.modal');
-// const modalImage = document.querySelector('.modal__image');
-// const modalClose = document.querySelector('.modal__close');
-
-// gallery.addEventListener('click', (event) => {
-//       event.preventDefault();
-
-//       if (event.target.classList.contains('gallery__image')) {
-//         const largeImageSrc = event.target.getAttribute('data-source');
-//         modalImage.src = largeImageSrc;
-//         modal.style.display = 'block';
-//       }
-//     });
-
-//     modalClose.addEventListener('click', () => {
-//       modal.style.display = 'none';
-//     });
-
-//     modal.addEventListener('click', (event) => {
-//       if (event.target === modal) {
-//         modal.style.display = 'none';
-//       }
-//     });
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery");
+const galleryItemEl = galleryItems.map(item => 
+  `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+    </a>
+  </li>`
+  ).join(" ");
+
+  galleryEl.insertAdjacentHTML ("beforeend", galleryItemEl);
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionData: "alt",
+    captionDelay: 250,
+  });
+
+
+
